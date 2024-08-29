74683ab35870578cac7c974707ef7cb92fe640d2
Saurish <saurishdarodkar@gmail.com>
2024-08-29 12:33:13 +0530

    Clean stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Layout from './components/layout/layout';
+import Layout from './components/Layout/layout';
 import AppRoutes from './Routes/Routes' 
 import { BrowserRouter } from 'react-router-dom';
 
